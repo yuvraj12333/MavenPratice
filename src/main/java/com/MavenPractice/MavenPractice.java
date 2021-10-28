9bca3f3e8caeb97262e4eee58b90a87921f73c83
yuvrajsingh123 <yuvi0622@gmail.com>
2021-10-28 12:53:07 +0530

    Practing Maven!!!!

---
+++ src/main/java/com/MavenPractice/MavenPractice.java
@@ -0,0 +1,8 @@
+package com.MavenPractice;
+
+public class MavenPractice {
+public static String Info() {
+	
+	return "Hello Bhaisaab";
+}
+}
---
+++ src/test/java/com/MavenPracticeTest/MavenPracticeTest.java
@@ -0,0 +1,13 @@
+package com.MavenPracticeTest;
+
+import org.junit.Test;
+
+import com.MavenPractice.MavenPractice;
+
+public class MavenPracticeTest {
+@Test
+	public void test() {
+	String hold=MavenPractice.Info();
+	System.out.println("Text is:-"+hold);
+}
+}
